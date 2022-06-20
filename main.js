65a866d998a30fa665d6500e5c7ab61d8e329d5c
jay2viv <97814633+jay2viv@users.noreply.github.com>
2022-06-20 10:53:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var username=""
+var password=""
+
+function addUser()
+{
+    localStorage.setItem("username");
+    localStorage.setItem("password");
+    window.location.replace("kondonasiaindex.html");
+}
